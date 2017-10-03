c7141bacaba2c1255e7f1e309b6a372ba3c160ea
TimeFur <djs860403@gmail.com>
2017-10-03 22:20:55 +0800

    plot the regreeion line

--- Agent/regression.py
+++ Agent/regression.py
@@ -22,7 +22,6 @@ def linear_regression(x, y):
     return res[a], res[b]
     
 def main():
-    linear_regression([1, 2], [2, 5])
     
     X = np.linspace(0, 10)
     f = lambda x: x
@@ -32,8 +31,19 @@ def main():
     random_sign = np.vectorize(lambda x: x if np.random.sample() > 0.5 else -x)
     data_X = np.linspace(1, 9, n)
     data_Y = random_sign(np.random.sample(n) * 2) + F(data_X)
+
     print data_X
     print data_Y
+    
+    a, b = linear_regression(data_X, data_Y)
+    f = lambda x: a * x + b
+    Fvec = np.vectorize(f)
+    regression_y = Fvec(data_X)
+
+    plt.plot(data_X, regression_y, 'g')
+    plt.plot(data_X, data_Y, 'ro')
+    
+    plt.show()
     
     
 if __name__ == "__main__":
