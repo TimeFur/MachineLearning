d55e5b50e7afca73f2e9eee243ca3ddc1a504d1d
TimeFur <djs860403@gmail.com>
2017-10-04 16:05:42 +0800

    Using the sklearn to implement svm

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ os.sys.path.append("./Agent/")
 from kmeans import *
 from knn import *
 from sklearn import tree
+from sklearn import svm
 
 '''----------------------
     Global variable
@@ -23,6 +24,25 @@ target = array(["A",
 test = array([6,9])
 k = 3
 
+Dataset = array([[2, 5, 'y'],
+                 [24, 5, 'y'],
+                 [2, 15, 'n'],
+                 [25, 51, 'n'],
+                 [6, 5, 'y']])
+Trainset = array([[9, 5],
+                 [4, 58],
+                 [2, 15],
+                 [25, 0],
+                 [6, 5]])
+
+def svm_run(dataset, trainset):
+    data_input = dataset[:,:-1]
+    target = dataset[:,-1]
+    
+    clf = svm.SVC()
+    clf.fit(data_input, target)
+    print clf.predict(trainset)
+    
 def main():
 
     #Kmeans
@@ -34,7 +54,9 @@ def main():
     print knn_machine.train()
 
     #Decision tree
-    
+
+    #SVM
+    svm_run(Dataset, Trainset)
     
 if __name__ == "__main__":
     main()
